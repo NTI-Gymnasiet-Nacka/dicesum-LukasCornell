62879b2548d7d535ed5f608a46aa12f635bdf385
Lukas Cornell <lukas.cornell@elev.ga.ntig.se>
2024-08-21 10:19:46 +0200

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-# Dice sum probability calculator
-# Författare: 
-# Datum:
 
 def main():
     user_input = input().split(" ")
+    user_input = user_input
+    if int(user_input[0]) == int(user_input[1]):
+        print(int(user_input[0])+1)
+    else:
+        if int(user_input[0]) >= int(user_input[1]):
+            for i in range(int(user_input[0]) - int(user_input[1])+1):
+                print(i + int(user_input[1])+1)
+        else:
+            for i in range(int(user_input[1]) - int(user_input[0])+1):
+             print(i + int(user_input[0])+1)
+        
+    
 
 if __name__ == "__main__":
     main()
+
